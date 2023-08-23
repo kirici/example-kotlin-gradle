98542c2f489e4fca46e3f1a6ad6126cc2a64a33c
C. Burak Kirici <krcdotbrk@gmail.com>
2023-08-23 13:09:13 +0200

    WIP: add checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.8.20"
     id("io.gitlab.arturbosch.detekt").version("1.23.1")
 
-    application 
+    application
+    checkstyle
 }
 
 repositories {
@@ -14,4 +15,17 @@ dependencies {
 
 application {
     mainClass.set("demo.AppKt") 
+}
+
+checkstyle {
+    toolVersion = "10.3.3"
+    isShowViolations = true
+    isIgnoreFailures = true
+}
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        sarif.required = true
+        xml.required = false
+        html.required = fase
+    }
 }
