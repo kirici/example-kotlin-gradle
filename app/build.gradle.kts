9fbaac16c25382386974fe8097e5f2efae53cd2b
C. Burak Kirici <krcdotbrk@gmail.com>
2023-08-23 13:44:49 +0200

    WIP: add diktat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
-plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.8.20"
-    id("io.gitlab.arturbosch.detekt").version("1.23.1")
+import com.saveourtool.diktat.plugin.gradle.DiktatExtension
 
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.8.22"
+    id("io.gitlab.arturbosch.detekt").version("1.23.1")
+    id("com.saveourtool.diktat.diktat-gradle-plugin") version "1.2.5"
     application
     checkstyle
 }
@@ -16,7 +18,9 @@ dependencies {
 application {
     mainClass.set("demo.AppKt") 
 }
-
+diktat {
+    inputs { include("app/src/**/*.kt") }
+}
 checkstyle {
     toolVersion = "10.3.3"
     isShowViolations = true
